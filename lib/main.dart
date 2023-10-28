ffdded2de5e891d995049dcab2cde5f5d613313b
Ichsan Ramadhan <ichsan.ramadhan32@gmail.com>
2023-10-28 20:21:43 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MamarecipeApp());
+}
+
+class MamarecipeApp extends StatelessWidget {
+  const MamarecipeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Mamarecipe",
+    );
+  }
+}
